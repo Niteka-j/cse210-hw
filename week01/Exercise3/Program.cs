5996d9487c14f39627bcc38307e84c7215e28034
jolis <bigirimanajolis0@gmail.com>
2026-09-02 11:09:07 +0200

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+        int guessCount = 0; // Track the number of attempts
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            // Basic input validation to prevent crashes if the user enters non-numbers
+            if (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+            // Increment the counter on every valid guess
+            guessCount++; 
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guessCount} guesses.");
+            }
+        }
     }
 }
