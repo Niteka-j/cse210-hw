5fd5ffef3374c6e3d0f4f9be773c2ad713c8567b
jolis <bigirimanajolis0@gmail.com>
2026-09-02 10:12:46 +0200

    Finished Exercise2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,7 +6,8 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         
-        // Ask the user for their first and last name, then display their name in the format "Last, First Last"
+// Ask the user for their first and last name, then display their name in the format "Last, First Last"
+
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // prompt the user for their grade percentage
+            
+            Console.Write("What is your grade percentage? ");
+            string answer = Console.ReadLine();
+            int percent = int.Parse(answer);
+
+            string letter = "";
+
+            if (percent >= 95)
+            {
+                letter = "A";
+            }
+            else if (percent >= 85)
+            {
+                letter = "B";
+            }
+            else if (percent >= 70)
+            {
+                letter = "C";
+            }
+            else if (percent >= 65)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            Console.WriteLine($"Your grade is: {letter}");
+            
+            if (percent >= 70)
+            {
+                Console.WriteLine("You passed!");
+            }
+            else
+            {
+                Console.WriteLine("Next time will be your time!");
+            }
+
+            // Prompt user to try again
+            Console.Write("\nWould you like to check another grade? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
     }
 }
